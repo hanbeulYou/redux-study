4ec52279b8335faa18b023dc8b565f162a822f6a
hanbeulYou <hanbeul.you@gmail.com>
2023-01-25 15:18:52 +0900

    feat: redux middleware 적용

--- react-redux-app/src/index.tsx
+++ react-redux-app/src/index.tsx
@@ -3,15 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './reducer';
 import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
+  );
+  
+  const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+    console.log("store", store);
+    console.log("action", action)
+    next(action);
+  }
+  
+  const middleware = applyMiddleware(loggerMiddleware);
+  const store = createStore(rootReducer, middleware)
+
 const render = () => root.render(
   <React.StrictMode>
     <Provider store={store}>
